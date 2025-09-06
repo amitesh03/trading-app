91ef40aead474360ad2165bfc2ede3d0dff786a6
Amitesh Patnaik <49634729+amitesh03@users.noreply.github.com>
2025-09-07 00:00:01 +0530

    adding poller

--- apps/frontend/app/dashboard/page.tsx
+++ apps/frontend/app/dashboard/page.tsx
@@ -1,19 +1,25 @@
 import React from 'react'
-
+import Charts from '@/components/Charts'
 function Dashboard() {
   return (
     <div>
       <h1 className='text-2xl font-medium font p-4'>
         Welcome Back!Alex.
 
-        Todo:
+<div>
+
+<Charts/>
+</div>
+      
+
+        {/* Todo:
         Total Portfolio Value
         24h P&L (Profit & Loss)
         Active Positions
         Cash Balance
         Recent activity feed (last few trades/orders)
         Quick actions (e.g., "Buy Bitcoin", "View Markets")
-        Mini charts (e.g., portfolio value trend over 7 days)
+        Mini charts (e.g., portfolio value trend over 7 days) */}
       </h1>
     </div>
   )
